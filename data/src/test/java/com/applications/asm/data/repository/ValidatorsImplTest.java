e4797e6d6c1692836603af5b21d2adcbcb820ce2
Arturo Sanchez <arturosanchez@Mac-mini-de-Arturo.local>
2022-10-08 09:29:07 -0500

    Se agregan los test para los validadores

--- data/src/test/java/com/applications/asm/data/repository/ValidatorsImplTest.java
+++ data/src/test/java/com/applications/asm/data/repository/ValidatorsImplTest.java
@@ -1,4 +1,66 @@
+package com.applications.asm.data.repository;
+
 import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class ValidatorsImplTest {
-  
+    private ValidatorsImpl validators;
+
+    @Before
+    public void setUp() {
+        validators = new ValidatorsImpl();
+    }
+
+    @Test
+    public void validatorsNotNullTest() {
+        assertNotNull(validators);
+    }
+
+    @Test
+    public void validateLatitudeIsValid() {
+        double latitude = 50.4;
+        assertTrue(validators.validateLatitudeRange(latitude));
+    }
+
+    @Test
+    public void validateLatitudeInvalid() {
+        double latitude = 90.1;
+        assertFalse(validators.validateLatitudeRange(latitude));
+    }
+
+    @Test
+    public void validateLongitudeValid() {
+        double longitude = 180.0;
+        assertTrue(validators.validateLongitudeRange(longitude));
+    }
+
+    @Test
+    public void validateLongitudeInvalid() {
+        double longitude = -180.1;
+        assertFalse(validators.validateLongitudeRange(longitude));
+    }
+
+    @Test
+    public void validateRadiusRangeValid() {
+        int radius = 1;
+        assertTrue(validators.validateRadiusRange(radius));
+    }
+
+    @Test
+    public void validateRadiusOutOfRange() {
+        int radius = -1;
+        assertFalse(validators.validateRadiusRange(radius));
+    }
+
+    @Test
+    public void pageIsLargerThanZero() {
+        assertTrue(validators.isValidPage(1));
+    }
+
+    @Test
+    public void pageIsLessThanZero() {
+        assertFalse(validators.isValidPage(0));
+    }
 }
